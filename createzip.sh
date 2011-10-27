7e7e2ff2a23e83190a0ace2428d62fa291ffac4e
Timo Schmid <tschmid@ernw.de>
2011-10-27 13:07:43 +0200

    zip wird von script geloescht

--- createzip.sh
+++ createzip.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-rm -f cvsscalc.zip
 zip cvsscalc.zip __main__.py cvsscalc/*.py
 
 echo '#!/usr/bin/env python2' > cvsscalc.zippy
 cat cvsscalc.zip >> cvsscalc.zippy
+rm -f cvsscalc.zip
 chmod 755 cvsscalc.zippy
