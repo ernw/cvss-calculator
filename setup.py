de91377d6e834b240f8e522d5270ec045104e0a4
Timo Schmid <tschmid@ernw.de>
2015-08-26 11:50:02 +0200

    meta files

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except:
+        return None
+
+requires=[
+        'wxpython',
+        ]
+
+setup(
+    name = "cvss_calculator",
+    version = "0.0.5",
+    author = "Timo Schmid",
+    author_email = "tschmid@ernw.de",
+    description = ("A CVSS 2 Calculator and Editor"),
+    license = "BSD",
+    keywords = "cvss calculator editor",
+    url = "",
+    install_requires = requires,
+    packages=[
+        'cvsscalc', 
+        #'tests'
+        ],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Information Technology",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    #data_files=[
+    #    ('share/applications', 'freedesktop/cvsscalc.desktop'),
+    #    ('share/mime/application', 'freedesktop/x-extension-cvss-mime.xml'),
+    #        ],
+)
